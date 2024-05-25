e87e6b0073f3dd018ab950cea0422ce9e096ffdb
sunny96087 <sunny96087@yahoo.com.tw>
2024-05-25 21:58:28 +0800

    feat: 新增 stores api 管理、新增通用元件
    1. stores apiService > 管理 api
    2. stores eventBus > 管理全局元件狀態
    3. 新增元件 Loading & Toast & Dialog
    4. app.vue 掛載通用元件

---
+++ pages/adminLogin/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { APIStore } from '~/stores/apiService'
+const store = APIStore()
+import { showToast, openDialog, showLoading, hideLoading } from '~/stores/eventBus'
+
+onMounted(() => {
+  console.log('adminLogin mounted')
+  // showLoading()
+})
+</script>
+<template>
+  <div>adminLogin</div>
+</template>
